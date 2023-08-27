f00ff3529ccfc9ccf3fb34e19ef368bbe49fe5af
Danila Deryavko <deryavkodan@gmail.com>
2023-08-28 03:20:11 +0700

    Added batch_monotime_recorder.py

--- src/main.py
+++ src/main.py
@@ -1,27 +1,42 @@
+import datetime
 import json
 import os.path
+from queue import Queue
 
 import cv2
 
-from motion_recorders.motion_recorder import MotionRecorder
+from motion_recorders.batch_monotime_recorder import BatchMonotimeMotionRecorder
 
-video_dir = os.environ['VideoDir']
-video_name = os.environ['VideoName']
-output_path = os.environ['OutputPath']
+# video_dir = os.environ['VideoDir']
+video_dir = 'media'
+video_names = os.environ['VideoNames']
+print(video_names)
+video_names = json.loads(video_names)
+
+# video_names = ['firstMonotimeTest.mp4', 'secondMonotimeTest.mp4']
+video_paths = Queue()
+for video_name in video_names:
+    path = os.path.join(video_dir, video_name)
+    print(path)
+    video_paths.put(path)
+output_path = 'render'
 #                       left_top    bottom_right
 # interesting_area = [(x_lt, y_lt), (x_br, y_br)]
 area_interest = json.loads(os.environ['AreaInterest'])
+print(area_interest)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 cap_fps = 5
 threshold = 0.5
 
-motion_recorder = MotionRecorder(os.path.join(video_dir, video_name),
-                                 output_path, kernel, area_interest, cap_fps, threshold)
+def calc_name():
+    return os.path.join(output_path, f'test_result {datetime.datetime.now().strftime("%H-%M-%S.%f")}.mp4')
+
+motion_recorder = BatchMonotimeMotionRecorder(video_paths, calc_name, kernel, area_interest, cap_fps, threshold,
+                                              datetime.timedelta(hours=1))
 try:
     motion_recorder.record()
 except Exception as e:
     pass
-pass
 #
 #
 #
--- src/motion_recorders/batch_monotime_recorder.py
+++ src/motion_recorders/batch_monotime_recorder.py
@@ -1,40 +1,38 @@
+import datetime
+from queue import Queue
+
 import cv2
 
 from capturer import VideoCapturer
+from motion_recorders.motion_recorder import MotionRecorder
 
 
-class MotionRecorder:
-    CODEC = 'mp4v'
-    FOURCC = cv2.VideoWriter_fourcc(*CODEC)
-
+class BatchMonotimeMotionRecorder(MotionRecorder):
     def __init__(
             self,
-            video_path,
-            output_path,
+            video_paths: Queue,
+            create_output_name_delegate,
             kernel,
-            area_interest,
+            area_of_interesting,
             cap_fps_movement,
-            threshold
-    ):
-        self._video_reader: VideoCapturer = None
-        self._video_writer: cv2.VideoWriter = None
-        self._required_frame_number = None
+            threshold,
+            output_time_limit: datetime.timedelta,
 
-        self._kernel = kernel
-        self._area_interest = area_interest
-        self._threshold = threshold
-        self._frame_number = 0
-        self._is_end_video = False
-        self._video_path = video_path
-        self._output_path = output_path
-        self._cap_fps_movement = cap_fps_movement
+    ):
+        super().__init__(video_paths.get(), create_output_name_delegate(), kernel, area_of_interesting,
+                         cap_fps_movement,
+                         threshold)
+        self._video_paths = video_paths
+        self._output_time_limit = output_time_limit
+        self._create_output_name_delegate = create_output_name_delegate
+        self._writen_frame_number = 0
+        self._output_video_fps = None
 
     def record(self):
         self._io_init()
         self._dependency_io_init()
         _, src_frame_t_minus, translated_frame_t_minus = self._read_first_checked_frame()
         _, src_frame_t, translated_frame_t = self._read_checked_frame()
-
 
         while not self._is_end_video:
             _, src_frame_t_plus, translated_frame_t_plus = self._read_checked_frame()
@@ -45,76 +43,75 @@ class MotionRecorder:
             if is_motion := mean_of_mask > self._threshold:
                 foreground_frame = translated_frame_t_minus
                 translated_frame_t_minus, translated_frame_t = translated_frame_t, translated_frame_t_plus
-                self._video_writer.write(src_frame_t_plus)
+                self._write(src_frame_t_plus)
                 while is_motion and not self._is_end_video:
                     _, src_frame_t_plus = self._read()
                     if self._is_end_video:
                         break
                     if self._frame_number % self._required_frame_number != 0:
-                        self._video_writer.write(src_frame_t_plus)
+                        self._write(src_frame_t_plus)
                         continue
                     translated_frame_t_plus = self.translate_frame(src_frame_t_plus)
                     mask_frame, mean_of_mask = self._difference_image(translated_frame_t_minus, translated_frame_t,
                                                                       translated_frame_t_plus)
                     is_motion = mean_of_mask > self._threshold
                     if is_motion:
-                        self._video_writer.write(src_frame_t_plus)
+                        self._write(src_frame_t_plus)
                     else:
                         mask_frame, mean_of_mask = self._difference_image(foreground_frame, translated_frame_t,
                                                                           translated_frame_t_plus)
                         is_motion = mean_of_mask > self._threshold
                         if is_motion:
-                            self._video_writer.write(src_frame_t_plus)
+                            self._write(src_frame_t_plus)
                     translated_frame_t_minus, translated_frame_t = translated_frame_t, translated_frame_t_plus
             else:
                 translated_frame_t_minus, translated_frame_t = translated_frame_t, translated_frame_t_plus
         self._release()
 
+    def _io_init(self):
+        super()._io_init()
+        self._output_video_fps = self._video_reader.fps
+
+    def _write(self, frame):
+        self._video_writer.write(frame)
+        self._writen_frame_number += 1
+        if self._writen_frame_number > self._output_time_limit.seconds * self._output_video_fps:
+            self._video_writer.release()
+            fps, width, height = self._video_reader.fps, self._video_reader.width, self._video_reader.height
+            self._output_path = self._create_output_name_delegate()
+            self._video_writer = cv2.VideoWriter(self._output_path, self.FOURCC, fps, (width, height))
+            self._output_video_fps = fps
+            self._writen_frame_number = 0
+
     def _read(self):
         retval, frame = self._video_reader.read()
         self._frame_number += 1
-        self._is_end_video = not retval
+        if not retval:
+            if self._open_next_video():
+                return self._read()
+            self._is_end_video = not retval
+            return retval, None
         return retval, frame
 
-    def _read_first_checked_frame(self):
-        retval, frame, translated_frame = self._read_checked_frame()
-        return retval, frame, translated_frame
-
     def _read_checked_frame(self):
-        while (is_not_end_video := self._video_reader.grab()) and self._frame_number % self._required_frame_number:
+        while (retval := self._video_reader.grab()) and self._frame_number % self._required_frame_number:
             self._frame_number += 1
-        self._is_end_video = not is_not_end_video
-        if self._is_end_video:
+        if not retval:
+            if self._open_next_video():
+                return self._read_checked_frame()
+            self._is_end_video = not retval
             return False, None, None
         retval, frame = self._video_reader.retrieve()
         self._frame_number += 1
         translated_frame = self.translate_frame(frame)
         return retval, frame, translated_frame
 
-    def _io_init(self):
+    def _open_next_video(self):
+        if self._video_paths.empty():
+            return False
+        self._video_reader.release()
+        self._video_path = self._video_paths.get()
         self._video_reader = VideoCapturer(self._video_path)
-        fps, width, height = self._video_reader.fps, self._video_reader.width, self._video_reader.height
-        self._video_writer = cv2.VideoWriter(self._output_path, self.FOURCC, fps, (width, height))
+        return True
 
-    def _dependency_io_init(self):
-        self._required_frame_number = self._video_reader.fps // self._cap_fps_movement
 
-    def _difference_image(self, frame_t_minus, frame_t, frame_t_plus):
-        delta_t_plus = cv2.absdiff(frame_t_plus, frame_t)
-        delta_t = cv2.absdiff(frame_t, frame_t_minus)
-        mask_frame = cv2.bitwise_and(delta_t_plus, delta_t)
-        mask_frame = cv2.morphologyEx(mask_frame, cv2.MORPH_OPEN, self._kernel)
-        return mask_frame, mask_frame.mean()
-
-    def translate_frame(self, frame):
-        frame = frame[
-                self._area_interest[0][1]:self._area_interest[1][1],
-                self._area_interest[0][0]:self._area_interest[1][0]
-                ]
-        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    def _release(self):
-        if self._video_writer and self._video_writer.isOpened():
-            self._video_writer.release()
-        if self._video_reader and self._video_reader.isOpened():
-            self._video_reader.release()
